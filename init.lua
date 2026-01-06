94950891dd21847faa0fb4e9d6319a2a80d43918
Joshua Lynch <Joshlynch2001@gmail.com>
2026-01-06 04:12:37 -0400

    added lsp and error handing

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 require("config.lazy")
 require("config.remap")
 require("config.set")
+
+
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate", -- optional but recommended
+    config = function()
+      require("mason").setup({
+        ui = {
+          border = "rounded",
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+}
+
