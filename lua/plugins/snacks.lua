17bd97857ac5446985ab19cd83ce4d06d10037b4
josh <joshlynch2001@gmail.com>
2026-02-08 17:52:14 -0400

    Finally added all the things required to have a custom header

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,44 +1,7 @@
 return {
 	"folke/snacks.nvim",
-	actions = {
-		open = {
-			close = true,
-		},
-	},
 	priority = 1000,
 	lazy = false,
-	opts = {
-		bigfile = { enabled = true },
-		dashboard = { enabled = true },
-		explorer = { enabled = true },
-		indent = { enabled = true },
-		input = { enabled = true },
-		notifier = {
-			enabled = true,
-			timeout = 3000,
-		},
-		picker = {
-			enabled = true,
-			defaults = {
-				close = true,
-			},
-			sources = {
-				explorer = {
-					auto_close = true,
-				},
-			},
-		},
-		quickfile = { enabled = true },
-		scope = { enabled = true },
-		scroll = { enabled = false },
-		statuscolumn = { enabled = true },
-		words = { enabled = true },
-		styles = {
-			notification = {
-				-- wo = { wrap = true } -- Wrap notifications
-			},
-		},
-	},
 	keys = {
 		-- Top Pickers & Explorer
 		{
@@ -513,44 +476,113 @@ return {
 			end,
 		},
 	},
-	init = function()
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "VeryLazy",
-			callback = function()
-				-- Setup some globals for debugging (lazy-loaded)
-				_G.dd = function(...)
-					Snacks.debug.inspect(...)
-				end
-				_G.bt = function()
-					Snacks.debug.backtrace()
-				end
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = {
+			preset = {
+				pick = nil,
+				---@type snacks.dashboard.Item[]
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = " ",
+						key = "c",
+						desc = "Config",
+						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+					},
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{
+						icon = "󰒲 ",
+						key = "l",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+				header = [[
+                                                                             
+               ████ ██████           █████      ██                     
+              ███████████             █████                             
+              █████████ ███████████████████ ███   ███████████   
+             █████████  ███    █████████████ █████ ██████████████   
+            █████████ ██████████ █████████ █████ █████ ████ █████   
+          ███████████ ███    ███ █████████ █████ █████ ████ █████  
+         ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+      ]],
+			},
+			sections = {
+				{ section = "header" },
+				{
+					section = "keys",
+					indent = 1,
+					padding = 1,
+				},
+				{ section = "recent_files", icon = " ", title = "Recent Files", indent = 3, padding = 2 },
+				{ section = "startup" },
+			},
+		},
+		explorer = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = false },
+		picker = { enabled = false },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		statuscolumn = { enabled = false },
+		words = { enabled = false },
+		rename = { enabled = true },
+		zen = {
+			enabled = true,
+			toggles = {
+				ufo = true,
+				dim = true,
+				git_signs = false,
+				diagnostics = false,
+				line_number = false,
+				relative_number = false,
+				signcolumn = "no",
+				indent = false,
+			},
+		},
+	},
+	config = function(_, opts)
+		require("snacks").setup(opts)
 
-				-- Override print to use snacks for `:=` command
-				if vim.fn.has("nvim-0.11") == 1 then
-					vim._print = function(_, ...)
-						dd(...)
-					end
+		Snacks.toggle.new({
+			id = "ufo",
+			name = "Enable/Disable ufo",
+			get = function()
+				return require("ufo").inspect()
+			end,
+			set = function(state)
+				if state == nil then
+					require("noice").enable()
+					require("ufo").enable()
+					vim.o.foldenable = true
+					vim.o.foldcolumn = "1"
 				else
-					vim.print = _G.dd
+					require("noice").disable()
+					require("ufo").disable()
+					vim.o.foldenable = false
+					vim.o.foldcolumn = "0"
 				end
-
-				-- Create some toggle mappings
-				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-				Snacks.toggle.diagnostics():map("<leader>ud")
-				Snacks.toggle.line_number():map("<leader>ul")
-				Snacks.toggle
-					.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-					:map("<leader>uc")
-				Snacks.toggle.treesitter():map("<leader>uT")
-				Snacks.toggle
-					.option("background", { off = "light", on = "dark", name = "Dark Background" })
-					:map("<leader>ub")
-				Snacks.toggle.inlay_hints():map("<leader>uh")
-				Snacks.toggle.indent():map("<leader>ug")
-				Snacks.toggle.dim():map("<leader>uD")
 			end,
 		})
 	end,
 }
+
+--
