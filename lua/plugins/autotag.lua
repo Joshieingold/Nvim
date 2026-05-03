1e178d310167c6ebbfea0fe8bc26dc87854aaac4
Joshua Lynch <JoshLynch2001@gmail.com>
2026-05-03 02:16:51 -0300

    added support for react

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,7 +1,10 @@
 return {
+	-- nvim-ts-autotag plugin
 	{
 		"windwp/nvim-ts-autotag",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
 		config = function()
 			require("nvim-ts-autotag").setup({
 				opts = {
@@ -9,12 +12,64 @@ return {
 					enable_rename = true,
 					enable_close_on_slash = false,
 				},
+				filetypes = {
+					"html",
+					"javascript",
+					"javascriptreact",
+					"typescriptreact",
+					"vue",
+					"svelte",
+					"tsx",
+					"jsx",
+				},
 				per_filetype = {
 					["html"] = {
-						enable_close = true,
+						enable_close = true, -- Enable auto-closing for HTML
+					},
+					["jsx"] = {
+						enable_close = true, -- Enable auto-closing for JSX
+					},
+					["tsx"] = {
+						enable_close = true, -- Enable auto-closing for TSX
+					},
+					["js"] = {
+						enable_close = true, -- Enable auto-closing for JS
 					},
 				},
 			})
+
+			-- nvim-treesitter configuration (ensure correct parsers are installed)
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "javascript", "typescript", "tsx", "html" },
+				highlight = {
+					enable = true, -- Enable Treesitter-based syntax highlighting
+				},
+				indent = {
+					enable = true, -- Enable Treesitter-based indentation
+				},
+			})
+		end,
+	},
+
+	{
+		"windwp/nvim-autopairs",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+		event = "InsertEnter",
+		config = function()
+			local autopairs = require("nvim-autopairs")
+			autopairs.setup({
+				check_ts = true,
+				ts_config = {
+					javascript = { "template_string" },
+					java = false,
+				},
+			})
+
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	},
 }
